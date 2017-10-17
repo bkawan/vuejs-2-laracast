5a53b563c6efed272d6151a432ea1d64c33571e6
bkawan <bikeshkawang@gmail.com>
2017-10-17 13:33:14 +0545

    add components within components

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+Vue.component('task-list', {
+    template: `
+  <div>
+        <task v-for="task in tasks">{{task.name}} - {{task.completed}}</task>
+    </div>
+  `,
+    data(){
+        return {
+            tasks: [
+                {name: 'EAt', completed: true},
+                {name: 'drink', completed: false},
+                {name: 'play', completed: true},
+                {name: 'dance', completed: false},
+                {name: 'watch', completed: true},
+                {name: 'sleep', completed: true},
+            ]
+
+        };
+    },
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
