9173a8fdf38b5c0c3f4273bfc07c976cb32f8900
bkawan <bikeshkawang@gmail.com>
2017-10-18 13:07:14 +0545

    hide with show directive and compoment

--- index1.js
+++ index1.js
@@ -5,19 +5,31 @@
 
 Vue.component('message', {
     props: ['title', 'body'],
+    data(){
+        return {
+            isVisible: true
+        };
+    },
     template: `
-        <div>
+        <div v-show="isVisible">
             <article class="message">
                 <div class="message-header">
                     <p>{{title}}</p>
-                    <button class="delete" aria-label="delete"></button>
+                    <button class="delete" @click='hideModal' aria-label="delete"></button>
                 </div>
                 <div class="message-body">
                  {{ body }} 
                 </div>
             </article>
         </div>
-        `
+        `,
+    methods: {
+        hideModal(){
+            this.isVisible = false;
+
+        },
+    }
+
 });
 
 var app1 = new Vue({
